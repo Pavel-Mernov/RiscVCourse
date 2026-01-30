3e777f94096a60883f570e2b661b00ae2b9b9d56
Павел Мернов <paulmernov@gmail.com>
2026-01-31 00:26:40 +0300

    injected prom metrics to compilation_service

--- compilation_service/src/main.ts
+++ compilation_service/src/main.ts
@@ -7,6 +7,7 @@ import * as fs from 'fs/promises';
 import { exec } from 'child_process';
 import multer, { diskStorage, memoryStorage } from 'multer';
 import logger from './logger/logger.js';
+import client from 'prom-client'
 
 
 const _dirname = process.cwd()
@@ -234,12 +235,39 @@ async function compileFile(req : RunFileRequest, res : Response) {
 }
 
 const app = express();
+const collectDefaultMetrics = client.collectDefaultMetrics;
 
+// Запускаем сбор стандартных метрик Node.js (CPU, память и т.д.)
+collectDefaultMetrics(); 
+
+// Создаём счётчик для запросов
+const httpRequestsCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status_code'],
+});
+
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestsCounter.inc({
+      method: req.method,
+      route: req.route ? req.route.path : req.path,
+      status_code: res.statusCode.toString(),
+    });
+  });
+  next();
+});
 
 const PORT = 3000;
 
 // app.use(bodyParser.text({ type: '*/*' }));
 app.use(express.json());
+
+// Маршрут для отдачи метрик в Prometheus-формате
+app.get('/metrics', async (_, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 app.post('/api/compile/file', upload.single('file'), compileFile);
 
