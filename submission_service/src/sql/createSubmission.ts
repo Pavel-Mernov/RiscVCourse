f87591b769115c699a1d1efc0b498cd4212351e1
Павел Мернов <paulmernov@gmail.com>
2026-02-27 12:50:25 +0300

    changed values in create submission method

--- submission_service/src/sql/createSubmission.ts
+++ submission_service/src/sql/createSubmission.ts
@@ -4,12 +4,12 @@ import { sqlPool } from "./sqlPool";
 
 export async function createSubmission(submission : Submission, connection : Pool | Client = sqlPool) {
     
-    // const { submission_id, task_id, student_id, timestamp, text, verdict } = submission
+    const { submission_id, task_id, student_id, timestamp, text, verdict } = submission
 
     const query = `
         INSERT INTO submissions(submission_id, task_id, student_id, timestamp, text, verdict)
         VALUES ($1, $2, $3, $4, $5, $6);
     `
 
-    await connection.query(query, Object.values(submission))
+    await connection.query(query, [submission_id, task_id, student_id, timestamp, text, verdict])
 }
