c946a70fdb329a85571e5e674893de5ff2aeb04c
Павел Мернов <paulmernov@gmail.com>
2026-05-14 14:26:32 +0300

    removed redundant console.log from frontend

--- client_app/src/components/urlText.tsx
+++ client_app/src/components/urlText.tsx
@@ -27,7 +27,7 @@ export default ({ children } : Props) => {
         }
         else if ('ul' in para) {
 
-          console.log('before', para.ul)
+          // console.log('before', para.ul)
 
           const parts = para.ul.map(li => 
             typeof li === 'string' 
@@ -41,7 +41,7 @@ export default ({ children } : Props) => {
             li 
           );
 
-          console.log('after', parts)
+          // console.log('after', parts)
 
           return (
             <Typography
