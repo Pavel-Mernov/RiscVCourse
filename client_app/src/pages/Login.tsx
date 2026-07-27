26943b2f6f8edbc3dcca98ce83d33e2a0b64ce9a
Павел Мернов <paulmernov@gmail.com>
2026-07-27 12:50:21 +0300

    added header content-type to login action in client_app

--- client_app/src/pages/Login.tsx
+++ client_app/src/pages/Login.tsx
@@ -43,6 +43,9 @@ export default () => {
             const response = await fetch(url, {
                 method: 'POST',
                 credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
                 body,
             })
             
