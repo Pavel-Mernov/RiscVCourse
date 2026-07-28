e041729482882307b440e9bcb34e3c8ea03300d2
Павел Мернов <paulmernov@gmail.com>
2026-07-28 10:41:27 +0300

    added secret key output in verifyToken, contest-service

--- contest_service/src/routes/verifyToken.ts
+++ contest_service/src/routes/verifyToken.ts
@@ -5,6 +5,10 @@ import { JWT_SECRET } from '../index';
 
 export async function verifyToken(token: string): Promise<JwtPayload | null> {
     try {
+
+        // temporary
+        console.log('JWT SECRET KEY: ' + JWT_SECRET)
+
         const payload = jwt.verify(token, JWT_SECRET);
 
         if (typeof payload === "string") {
